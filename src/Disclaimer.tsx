e2e0a7f9119313bb4eb3534d95ed5f6045d45870
giovanni <gioandro91@gmail.com>
2024-02-07 10:20:12 +0100

    updated Disclaimer and Example

--- src/Disclaimer.tsx
+++ src/Disclaimer.tsx
@@ -16,13 +16,16 @@ function Disclaimer() {
                     to decide whether the concept satisfy the given category and criteria.
                 </p>
                 <p className="mt-2">
-                    At any point during the annotation task, you can mark a concept as "<i>wrong</i>" if you
-                    find it erroneous. An erroneous concept coul be a truncated word (e.g., "t-shi") or a fragment
-                    of text (e.g., "that satisfy the").
+                    At any point during the annotation task, you can mark an information as "<i>wrong</i>" if you
+                    find it erroneous. A wrong semantic information could be an incomplete word (e.g., "t-shi")
+                    or a fragment of text (e.g., "that satisfy the").
                 </p>
+                <p>Please, register your name as <b>name_surname</b>.
+                    The registration is only used to pause the annotation.
+                    You can continue the annotation in a second moment from the point you interrupted. </p>
                 <p className="mt-2">
-                    You will need to complete <b>1000</b> of these <b>annotations</b>, which should take about <b>1
-                    hour</b>. It's important to note that this task
+                    You will need to complete <b>300</b> of these <b>annotations</b>, which should take about
+                    <b>30 minutes</b>. It's important to note that this task
                     does not evaluate your knowledge of English, so you don't need to worry about being tested. If
                     you need more information about the word, you can use the internet to help you.
                 </p>
--- src/Example.tsx
+++ src/Example.tsx
@@ -6,7 +6,7 @@ function Example() {
             <div className="text-4xl font-bold">Some Examples of annotation</div>
             <div>
 
-              <div className="text-2xl mt-4">Correct Concept</div>
+              <div className="text-2xl mt-4">Correct Semantic Information</div>
                 <p className="mt-2">
                     For instance, given the triplet
                     <ul>
@@ -17,7 +17,7 @@ function Example() {
                     the answer is <b>Yes</b> because the mosquito is an animal that flies.
                 </p>
 
-                <div className="text-2xl mt-4">Incorrect Concept</div>
+                <div className="text-2xl mt-4">Incorrect Semantic Information</div>
                 <p className="mt-2">
                     For instance, given the triplet
                     <ul>
@@ -28,9 +28,9 @@ function Example() {
                     the answer is <b>No</b> because superman is not a vehicle made of steel.
                 </p>
 
-                <div className="text-2xl mt-4">Wrong Concept</div>
+                <div className="text-2xl mt-4">Wrong Semantic Information</div>
                 <p className="mt-2">
-                    At any point during the annotation task, you can mark a concept as "<i>wrong</i>" if you
+                    At any point during the annotation task, you can mark an information as "<i>wrong</i>" if you
                     find it erroneous. For instance, given the triplet
                     <ul>
                         <li>Concept: <i>kang</i> </li>
